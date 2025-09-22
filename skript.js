dd7cbb64f0986ef482f5269baa1b39212a7c902d
kancharlamanojkumar <kancharlamanojkumar74@gmail.com>
2025-09-22 18:45:20 +0530

    Initial commit

--- skript.js
+++
@@ -1,7 +0,0 @@
-// Select the button by ID
-const button = document.getElementById("myButton");
-
-// Add click event listener
-button.addEventListener("click", function() {
-alert("Hello! You clicked the button 🚀");
-});
